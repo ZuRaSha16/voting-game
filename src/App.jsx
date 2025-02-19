30b4dff82e3427ba803b904b6552a9091b92e916
Zura Shukakidze <zurashukakidze85@gmail.com>
2025-02-19 11:34:35 +0400

    imported the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import React, { useState } from 'react' 
+
 function App() {
+  const [jokes, setJokes] = useState([])
+
+  const FetchJokes = () => {
+    const res= fetch(" https://teehee.dev/api/joke")
+    const data = res.json()
+    setJokes(data)
+  }
+
   return (
     <div className="text-center w-96 h-auto p-8 bg-blue-400 rounded-lg my-40 mx-auto text-white">
       <h1 className="text-2xl">Get a new joke😂</h1>
       <p className="text-l">New jokes upcoming</p>
-      <button className="bg-white text-black rounded-sm">New Joke</button>
+      <button onClick={FetchJokes} className="bg-white text-black rounded-sm">New Joke</button>
     </div>
   )
 }
