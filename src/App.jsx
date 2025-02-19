81904e250be581e2f53e237f525f638f55ed3e76
Zura Shukakidze <zurashukakidze85@gmail.com>
2025-02-19 11:30:49 +0400

    setting up the main page with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div className="text-center w-96 h-auto p-8 bg-blue-400 rounded-lg my-40 mx-auto text-white">
-      <h1>Get a new joke</h1>
-      <p>New jokes upcoming</p>
+      <h1 className="text-2xl">Get a new joke😂</h1>
+      <p className="text-l">New jokes upcoming</p>
       <button className="bg-white text-black rounded-sm">New Joke</button>
     </div>
   )
