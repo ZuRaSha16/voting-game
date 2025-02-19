1faa43669f9d2e492873baa7623b438289e9971c
Zura Shukakidze <zurashukakidze85@gmail.com>
2025-02-19 11:28:58 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center w-96 h-auto p-8 bg-blue-400 rounded-lg my-40 mx-auto text-white">
+      <h1>Get a new joke</h1>
+      <p>New jokes upcoming</p>
+      <button className="bg-white text-black rounded-sm">New Joke</button>
+    </div>
+  )
+}
+
+export default App
