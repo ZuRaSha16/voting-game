a5b5934e66388d62458d507423b6d137eef4ca6d
ZuRaSha16 <zurashukakidze85@gmail.com>
2025-02-25 00:20:04 +0400

    changed the site logo, name and repo page

--- src/components/JokeComponent.jsx
+++ src/components/JokeComponent.jsx
@@ -32,7 +32,7 @@ function JokeComponent() {
 
   return (
     <div className="text-center w-96 h-auto p-8 bg-white rounded-lg shadow-xl my-40 mx-auto text-black transition-all duration-300 hover:shadow-2xl">
-      <h1 className="text-2xl border-2 rounded-lg p-3 bg-gray-800 text-white mb-6 transition-colors duration-300 hover:bg-gray-700">Get a new joke 😂</h1>
+      <h1 className="text-2xl border-2 rounded-lg p-3 bg-gray-800 text-white mb-6 transition-colors duration-300 hover:bg-gray-700">JokeNator</h1>
       <div className="min-h-[100px] flex items-center justify-center">
         {joke ? (
           <p className="text-lg transition-opacity duration-300 ease-in-out">{joke}</p>
